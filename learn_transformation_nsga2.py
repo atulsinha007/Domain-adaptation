36776b83ed7c528c6da02850a2f5c732ddb8f04c
nilinswap <nilinswap@gmail.com>
2018-04-15 22:42:44 +0530

    important commit of the branch

--- learn_transformation_nsga2.py
+++ learn_transformation_nsga2.py
@@ -125,24 +125,24 @@ def closeness_cost(ind):
 	# print( source_dic)
 
 	#print(target_rest_label)
-	for target_instance, target_lab in zip(target_rest_arr, target_rest_label):
+	for source_instance, source_lab in zip(source_rest_arr, source_rest_label):
 		# assert (target_instance in source_rest_arr)
 		
 		min_dist = np.inf
-		target_instance = np.reshape(target_instance, (target_instance.shape[0], 1))
+		source_instance = np.reshape(source_instance, (source_instance.shape[0], 1))
 		
 
-		transformed_target = np.dot(W, target_instance)
-		transformed_target = np.ravel(transformed_target)
+		transformed_source = np.dot(W, source_instance)
+		transformed_source = np.ravel(transformed_source)
 
 		#assert (transformed_target in source_rest_arr)
 		#print(source_rest_arr.shape)
-		for source_instance, source_lab in zip(source_rest_arr, source_rest_label):
+		for target_instance, target_lab in zip(target_rest_arr, target_rest_label):
 			#print("here")
 			# print(transformed_target, source_instance)
 			#print(source_instance)
 			if source_lab == target_lab:
-				min_dist = min(min_dist, dist(transformed_target, source_instance))
+				min_dist = min(min_dist, dist(transformed_source, target_instance))
 			#print(min_dist)
 		sumi += min_dist
 
